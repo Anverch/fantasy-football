fbe8c93fabf0fff682826116c59f283fbc26d1da
Aleksandr Saiyan <anverch@gmail.com>
2020-08-28 19:18:33 -0500

    Updates

---
+++ precache-manifest.0cf8febf2182e1198742ca2b266b63a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d03ed3f42feae1685627b041cb787f",
+    "url": "/fantasy-football/index.html"
+  },
+  {
+    "revision": "f92a2c004545d3fd55d3",
+    "url": "/fantasy-football/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "802737ce69aaf628fe21",
+    "url": "/fantasy-football/static/js/2.f7e3edb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fantasy-football/static/js/2.f7e3edb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92a2c004545d3fd55d3",
+    "url": "/fantasy-football/static/js/main.bb9b2c30.chunk.js"
+  },
+  {
+    "revision": "50400c4fe162ffe5e39a",
+    "url": "/fantasy-football/static/js/runtime-main.410aae55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fantasy-football/static/media/logo.5d5d9eef.svg"
+  }
+]);
