f60b58d65cf9f52362d2e54737763c83a8d942a1
Aleksandr Saiyan <anverch@gmail.com>
2020-08-30 17:20:41 -0500

    add side menu button

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,14 @@
 import React from "react";
+
+import SideMenuButton from "../SideMenu/SideMenuButton"
 import "./Navigation.css"
 
 const navigation = props => (
     <header className="navigation">
         <nav className="navigation_tool">
-            <div></div>
+            <div>
+                <SideMenuButton />
+            </div>
             <div className="nav_logo">Saiyan League</div>
             <div className="spacer"></div>
             <div className="nav_tool_items">
---
+++ src/components/SideMenu/SideMenuButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SideMenuButton.css";
+
+const sideMenuButton = props => (
+    <button className="menu_button">
+        <div className="menu_button_line"/>
+        <div className="menu_button_line"/>
+        <div className="menu_button_line"/>
+    </button>
+);
+
+export default sideMenuButton;
