8011f8a8e428194e917547e28e8d6d4d7f786ae1
Aleksandr Saiyan <anverch@gmail.com>
2020-08-30 16:53:55 -0500

    nav js and css

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Navigation.css"
+
+const navigation = props => (
+    <header className="navigation">
+        <nav className="navigation_tool">
+            <div></div>
+            <div className="nav_logo">Saiyan League</div>
+            <div className="spacer"></div>
+            <div className="nav_tool_items">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">All-Time</a></li>
+                    <li><a href="/">Records</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default navigation;
